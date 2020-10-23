81cf074184b1cad693fc1789f91cf2fb5951df8d
RootEval <44101973+RootEval@users.noreply.github.com>
2020-10-23 17:02:58 +0300

    Key names changed

--- Server CS/Server CS/Message.cs
+++ Server CS/Server CS/Message.cs
@@ -4,13 +4,13 @@ namespace Server_CS
 {
     public class Message
     {
-        public int TimesTamp { get; set; }
+        public int Ts { get; set; }
         public string Name { get; set; }
         public string Text { get; set; }
 
         public override string ToString()
         {
-            return $"[{ (new DateTime(1970, 1, 1, 0, 0, 0, 0)).AddSeconds(TimesTamp)}] {Name}: {Text}";
+            return $"[{ (new DateTime(1970, 1, 1, 0, 0, 0, 0)).AddSeconds(Ts)}] {Name}: {Text}";
         }
     }
 }
