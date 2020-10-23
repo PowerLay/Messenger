f1178b8e3ff84625eeeed9378d4bf1adb2c0ead2
PowerLay <34715449+PowerLay@users.noreply.github.com>
2020-10-23 17:24:35 +0300

    time > ts

--- Client CS CLI/Client CS CLI/Message.cs
+++ Client CS CLI/Client CS CLI/Message.cs
@@ -4,20 +4,20 @@ namespace Client_CS_CLI
 {
     /// <summary>
     /// <para>Класс Сообщение</para>
-    /// <br>MsgTime - время отправки сообщения (по серверу)</br>
+    /// <br>Ts - время отправки сообщения (по серверу)</br>
     /// <br>Name - имя клиента</br>
-    /// <br>Msg - сообщение клиента</br>
+    /// <br>Text - сообщение клиента</br>
     /// <br>ToString - функция преобразования полей класса в строку для печати</br>
     /// </summary>
     public class Message
     {
-        public int TimesTamp { get; set; }
+        public int Ts { get; set; }
         public string Name { get; set; }
         public string Text { get; set; }
 
         public override string ToString()
         {
-            return $"[{ (new DateTime(1970, 1, 1, 0, 0, 0, 0)).AddSeconds(TimesTamp)}] {Name}: {Text}";
+            return $"[{ (new DateTime(1970, 1, 1, 0, 0, 0, 0)).AddSeconds(Ts)}] {Name}: {Text}";
         }
     }
 }
