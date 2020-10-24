db2fabfe1286dd295ee34f290958260f2b6fb031
PowerLay <34715449+PowerLay@users.noreply.github.com>
2020-10-24 12:59:48 +0300

    fixed GET response

--- CLient_CS_UWP/CLient_CS_UWP/MainPage.xaml.cs
+++ CLient_CS_UWP/CLient_CS_UWP/MainPage.xaml.cs
@@ -1,31 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.System;
-using Windows.UI.Xaml;
+using Windows.UI.Core;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.Web.Http.Headers;
+using Windows.UI.Xaml.Input;
 using Newtonsoft.Json;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
 namespace CLient_CS_UWP
 {
+    internal class ServerResponse
+    {
+        private readonly MainPage mainPage;
 
+        public ServerResponse(MainPage mp)
+        {
+            mainPage = mp;
+        }
+
+        private async Task GetHistory()
+        {
+            await mainPage.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => { mainPage.UpdateHistory(); });
+        }
+
+        public async void Start()
+        {
+            while (true)
+            {
+                await GetHistory();
+                Thread.Sleep(200);
+            }
+        }
+    }
 
     /// <summary>
-    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    ///     Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
     /// </summary>
     public sealed partial class MainPage : Page
     {
@@ -34,38 +48,37 @@ namespace CLient_CS_UWP
 
         public MainPage()
         {
-            this.InitializeComponent();
-            GetHistoryMessages();
+            InitializeComponent();
+            var sr = new ServerResponse(this);
+            var updaterThread = new Thread(sr.Start);
+            updaterThread.Start();
             //ConfigManager.LoadConfig();
-        }
-        private async void GetHistoryMessages() 
-        {
-            //DateTime time = new DateTime();
-            while (true)
-            {
-                //if (time.AddMilliseconds(200) < DateTime.Now)
-                //{
-                await UpdateHistory();
-                //    time = DateTime.Now;
-                //}
-            }
         }
 
         public async Task<string> GetAsync(string uri)
         {
-            var request = (HttpWebRequest)WebRequest.Create(uri);
+            var request = (HttpWebRequest) WebRequest.Create(uri);
             request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
 
-            using (var response = (HttpWebResponse)await request.GetResponseAsync())
+            using (var response = (HttpWebResponse) await request.GetResponseAsync())
             using (var stream = response.GetResponseStream())
             using (var reader = new StreamReader(stream))
             {
                 return await reader.ReadToEndAsync();
             }
         }
-        private async Task UpdateHistory()
+
+        public async void UpdateHistory()
         {
-            var res = await GetAsync("http://localhost:5000/api/Chat");
+            string res;
+            try
+            {
+                res = await GetAsync("http://localhost:5000/api/Chat");
+            }
+            catch (Exception)
+            {
+                return;
+            }
 
             if (res != "[]" || res != prevRes)
             {
@@ -73,22 +86,21 @@ namespace CLient_CS_UWP
 
                 if (_len != messages.Count)
                 {
-                    for (int i = _len; i < messages.Count; i++)
-                    {
+                    for (var i = _len; i < messages.Count; i++)
                         MessagesListViev.Items.Add(messages[i].ToString());
-                        //MessagesListViev.Items.Add(new TextBlock() { Text = messages[i].ToString() });
-                    }
 
                     _len = messages.Count;
                 }
+
                 prevRes = res;
             }
         }
+
         private void Post(string nick)
         {
             var msg = MessageBox.Text;
 
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:5000/api/Chat");
+            var httpWebRequest = (HttpWebRequest) WebRequest.Create("http://localhost:5000/api/Chat");
             httpWebRequest.ContentType = "application/json";
             httpWebRequest.Method = "POST";
 
@@ -98,7 +110,7 @@ namespace CLient_CS_UWP
 
         private void GetAnswer(HttpWebRequest httpWebRequest)
         {
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            var httpResponse = (HttpWebResponse) httpWebRequest.GetResponse();
             var streamReader = new StreamReader(httpResponse.GetResponseStream());
             var result = streamReader.ReadToEnd();
             if (result != "ok") Console.WriteLine("Something went wrong");
@@ -106,21 +118,20 @@ namespace CLient_CS_UWP
 
         private static void SendMessage(string nick, string msg, HttpWebRequest httpWebRequest)
         {
-            var json = JsonConvert.SerializeObject(new Message { Name = nick, Msg = msg });
+            var json = JsonConvert.SerializeObject(new Message {Name = nick, Text = msg});
             var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream());
             streamWriter.Write(json);
             streamWriter.Close();
         }
 
-        private void MessageBox_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
+        private void MessageBox_KeyDown(object sender, KeyRoutedEventArgs e)
         {
             if (e.Key == VirtualKey.Enter)
             {
-                string nick = "Test";
+                var nick = "Test";
                 Post(nick);
                 MessageBox.Text = "";
             }
         }
-
     }
 }
