512fcaffb20b6bdccad1e0882b8007b95bf8d7c8
PowerLay <34715449+PowerLay@users.noreply.github.com>
2020-10-24 15:26:32 +0300

    fixed config

--- CLient_CS_UWP/CLient_CS_UWP/ConfigManager.cs
+++ CLient_CS_UWP/CLient_CS_UWP/ConfigManager.cs
@@ -1,42 +1,60 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 using Newtonsoft.Json;
 
 namespace CLient_CS_UWP
 {
     public class ConfigManager
     {
-        public static Config config = new Config();
+        public static Config Config = new Config();
 
-        public static void LoadConfig()
+        private static async void WriteConfig(string path)
         {
-            var path = @"C:\config.json";
+            // Create sample file; replace if exists.
+            var storageFolder =
+                ApplicationData.Current.LocalFolder;
+            var sampleFile =
+                await storageFolder.CreateFileAsync(path,
+                    CreationCollisionOption.ReplaceExisting);
+            //var sf = await Package.Current.InstalledLocation.TryGetItemAsync(path) as StorageFile;
 
-            if (!File.Exists(path))
+            var stream = await sampleFile.OpenStreamForWriteAsync();
+            using (var streamWriter = new StreamWriter(stream))
             {
-                using (var streamWriter = new StreamWriter(path))
-                {
-                    streamWriter.Write(JsonConvert.SerializeObject(config));
-                }
-
-                return;
-            }
-
-            using (var streamReader = new StreamReader(path))
-            {
-                config = JsonConvert.DeserializeObject<Config>(streamReader.ReadToEnd());
+                await streamWriter.WriteAsync(JsonConvert.SerializeObject(Config));
             }
         }
+
+        public static async void LoadConfig()
+        {
+            var path = @"config.json";
+            var text = "";
+            try
+            {
+                var storageFolder =
+                    ApplicationData.Current.LocalFolder;
+                var sampleFile =
+                    await storageFolder.GetFileAsync(path);
+                text = await FileIO.ReadTextAsync(sampleFile);
+            }
+            catch (Exception)
+            {
+                WriteConfig(path);
+            }
+
+            if (text != "")
+                Config = JsonConvert.DeserializeObject<Config>(text);
+
+            if (Config == null) Config = new Config();
+        }
     }
+
     public class Config
     {
         public int MillisecondsSleep { get; set; } = 200;
         public bool AskNick { get; set; } = true;
         public string Name { get; set; } = "anonymous";
-
     }
 }
